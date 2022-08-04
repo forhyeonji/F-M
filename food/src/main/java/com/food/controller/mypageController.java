6fae4028f8eac7c78e784c41a4d32d0b09f4c0c3
kimseongmin0301 <hippo159753@gmail.com>
2022-08-04 14:29:24 +0900

    [김성민] 어노테이션 미입력 수정, UTF-8로 변경

--- food/src/main/java/com/food/controller/mypageController.java
+++ food/src/main/java/com/food/controller/mypageController.java
@@ -1,8 +1,10 @@
 package com.food.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+@Controller
 public class mypageController {
 
 	
