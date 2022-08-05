a79fca32f241148f414230986fe07d4fb7785915
kimseongmin0301 <hippo159753@gmail.com>
2022-08-05 10:32:08 +0900

    [김성민] boardController 메서드 추가

--- food/src/main/java/com/food/controller/boardController.java
+++ food/src/main/java/com/food/controller/boardController.java
@@ -7,9 +7,35 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class boardController {
 	
-	@RequestMapping(value = "/board", method = RequestMethod.GET)
-	public String list () {
-		return "Board/board";
+	@RequestMapping(value = "/noodle", method = RequestMethod.GET)
+	public String noodle () {
+		return "/Board/noodle";
 	}
+
+	@RequestMapping(value = "/bread", method = RequestMethod.GET)
+	public String bread () {
+		return "/Board/bread";
+	}
+
+	@RequestMapping(value = "/chinese", method = RequestMethod.GET)
+	public String chinese () {
+		return "/Board/chinese";
+	}
+
+	@RequestMapping(value = "/korean", method = RequestMethod.GET)
+	public String korean () {
+		return "/Board/korean";
+	}
+
+	@RequestMapping(value = "/meat", method = RequestMethod.GET)
+	public String meat () {
+		return "/Board/meat";
+	}
+
+	@RequestMapping(value = "/western", method = RequestMethod.GET)
+	public String western () {
+		return "/Board/western";
+	}
+
 
 }
