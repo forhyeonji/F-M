576f8c46703630905c4df3186249b369b55ad367
kimseongmin0301 <pose1358@naver.com>
2022-08-17 14:45:26 +0900

    게시판 조회수 증가, 페이징구현

---
+++ food/src/main/java/com/food/model/CriteriaVO.java
@@ -0,0 +1,40 @@
+package com.food.model;
+
+// pageNum과 amount(한페이지당 게시물 개수) 값을 전달하는 Model
+public class CriteriaVO {
+    private int pageNum;
+    private int amount;
+
+    public CriteriaVO(int pageNum, int amount) {
+        this.pageNum = pageNum;
+        this.amount = amount;
+    }
+
+    public CriteriaVO(){
+        this(1,10);
+    }
+
+    @Override
+    public String toString() {
+        return "CriteriaVO{" +
+                "pageNum=" + pageNum +
+                ", amount=" + amount +
+                '}';
+    }
+
+    public int getPageNum() {
+        return pageNum;
+    }
+
+    public void setPageNum(int pageNum) {
+        this.pageNum = pageNum;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+}
