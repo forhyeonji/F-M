53c9743b72368d60ed5d56ace0136c519e89eace
GR <GR@DESKTOP-ODLO1F7>
2022-08-17 16:51:00 +0900

    [이채현]"shop 전체 수정"

--- food/src/main/java/com/food/controller/ShopController.java
+++ food/src/main/java/com/food/controller/ShopController.java
@@ -1,29 +1,25 @@
 package com.food.controller;
 
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-
 @Controller
 public class ShopController {
-	
-	
-	
-   @RequestMapping(value = "/shop", method = RequestMethod.GET)
-   public String list () {
-      return "Shop/shop";
-   }
 
-   @RequestMapping(value = "/shopconer", method = RequestMethod.GET)
-   public String Coner () {
-      return "Shop/shopconer";
-   }
-  
-   @RequestMapping(value = "/shopbeef", method = RequestMethod.GET)
-   public String serve () {
-      return "Shop/shopbeef";
-   }
+	@RequestMapping(value = "/shop", method = RequestMethod.GET)
+	public String list() {
+		return "Shop/shop";
+	}
+
+	@RequestMapping(value = "/shopconer", method = RequestMethod.GET)
+	public String Coner() {
+		return "Shop/shopconer";
+	}
+
+	@RequestMapping(value = "/shopbeef", method = RequestMethod.GET)
+	public String serve() {
+		return "Shop/shopbeef";
+	}
 
 }
