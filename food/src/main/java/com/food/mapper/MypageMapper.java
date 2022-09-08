8b94e936e7951df50af90962bb6a2547e46dcc21
ringring <ringring395@gmail.com>
2022-09-08 14:05:33 +0900

    [김수진] 마이페이지 / 내글보기 & 회원정보수정

--- food/src/main/java/com/food/mapper/MypageMapper.java
+++ food/src/main/java/com/food/mapper/MypageMapper.java
@@ -2,7 +2,6 @@ package com.food.mapper;
 
 import java.util.ArrayList;
 
-import com.food.model.CriteriaVO;
 import com.food.model.MypageVO;
 import com.food.model.UserVO;
 
@@ -12,8 +11,17 @@ public interface MypageMapper {
 	//마이페이지를 출력하기 위한 DB 작업
 	public UserVO mypage(UserVO user);
 	
+	//회원정보를 출력하기 위한 DB작업
+	public UserVO profile(UserVO user);
+	
+	//회원정보를 수정하기 위한 DB작업
+	public void edit(UserVO user);
+	
+	//회원탈퇴를 하기 위한 DB작업
+	public void resignPost(UserVO user);
+	
 	//내가쓴글을 출력하기위한 DB작업
-	public ArrayList<MypageVO> mywrite(CriteriaVO cri);
+	public ArrayList<MypageVO> mywrite(MypageVO mypage);
 	
 	//게시판 전체 건수 조회하는 DB작업
 	public int total(UserVO user);
