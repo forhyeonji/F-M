15507f681e7aa257545e34de93d89e21c5cfd2b1
forhyeonji <ghkdguswl3@gmail.com>
2022-09-22 11:16:58 +0900

    [황현지]criteriaVO 수정취소

--- food/src/main/webapp/resources/JS/Notice/DirectSelect.js
+++ food/src/main/webapp/resources/JS/Notice/DirectSelect.js
@@ -2,40 +2,42 @@
  * 
  */
 
+
 $(document).ready(function(){
-	
-	
-	var sub =$("#subselect option:selected").val();
-	
+
 	
 		$("#subselect").on("change", function(){
+			
+			
+			var sub=$("#subselect option:selected").val();
 			
 			if(sub=='inquiry_recipe'){
 				
 				location.href="/directKing?sep=inquiry&subsep=inquiry_recipe";
-				$('input[name=subwhat]').attr('value', sub);
 				
 			}else if (sub=='inquiry_store'){
 				
 				location.href="/directKing?sep=inquiry&subsep=inquiry_store";
-				
-				
+						
 			}else if (sub=='inquiry_comm'){
 				
 				location.href="/directKing?sep=inquiry&subsep=inquiry_comm";
 				
-			
 			}else if (sub=='inquiry_ect'){
-			
+				
 				location.href="/directKing?sep=inquiry&subsep=inquiry_ect";
 			
-				
 			}
 			
-	
+		var result=sessionStorage.getItem(ab);
+		
+			
 			
 		})
 
+		
+		
+		
 		
 		$(".nowpage").on("click", function(){
 		
