2d67b21eb84c8c4dd4291044824b6058a6510e26
kimseongmin0301 <pose1358@naver.com>
2022-09-22 11:53:35 +0900

    [김성민] 좋아요 기능 구현

--- food/src/main/webapp/resources/JS/Board/BoardDetail.js
+++ food/src/main/webapp/resources/JS/Board/BoardDetail.js
@@ -136,6 +136,7 @@ $(function () {
                         onEmptyVote();
                     }
                 })
+                onVoteCount();
             }
             onCheckVote();
             onVoteCount();
