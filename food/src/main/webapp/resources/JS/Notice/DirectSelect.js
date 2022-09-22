777c3eb386472a98161ec0239f054226414f801a
forhyeonji <ghkdguswl3@gmail.com>
2022-09-22 11:31:31 +0900

    [황현지] 분류별 게시글 페이징 성공

--- food/src/main/webapp/resources/JS/Notice/DirectSelect.js
+++ food/src/main/webapp/resources/JS/Notice/DirectSelect.js
@@ -8,7 +8,7 @@ $(document).ready(function(){
 	
 		$("#subselect").on("change", function(){
 			
-			
+			/*
 			var sub=$("#subselect option:selected").val();
 			
 			if(sub=='inquiry_recipe'){
@@ -27,27 +27,15 @@ $(document).ready(function(){
 				
 				location.href="/directKing?sep=inquiry&subsep=inquiry_ect";
 			
-			}
-			
-		var result=sessionStorage.getItem(ab);
+			}*/
 		
+		$("input[name='pageNum']").val("1");
+		// form태그를 submit
+		$("#searchForm").submit();
 			
 			
 		})
 
-		
-		
-		
-		
-		$(".nowpage").on("click", function(){
-		
-			alert("주소좀 보자")
-			var url = $(".nowpage").attr('href');
-			
-			url = url.replace('#','여기되나?')
-			alert(url+"주소");
-			
-		})
 		
 		
 		
