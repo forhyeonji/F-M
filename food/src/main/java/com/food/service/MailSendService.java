bc286e6316309bcb366e2db5b2a8d20d74a0ff14
LeeYoung1n <cdcw2023@gmail.com>
2022-09-26 10:09:17 +0900

    [이영인] 메일 인증 서비스 진행 중.. pom.xml과 root context, servlet 건드림

--- food/src/main/java/com/food/service/MailSendService.java
+++ food/src/main/java/com/food/service/MailSendService.java
@@ -10,6 +10,7 @@ import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
 
+
 @Component
 public class MailSendService {
 	
